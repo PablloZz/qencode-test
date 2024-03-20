97f91d1ef464cf701ca95d4d9da73db158dd449f
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-03-20 15:20:21 +0200

    feat: Add input components re-export

--- src/libs/components/components.ts
+++ src/libs/components/components.ts
@@ -1,2 +1,4 @@
 export { App } from "./app/app.tsx";
+export { Input } from "./input/input.tsx";
+export { PasswordInput } from "./password-input/password-input.tsx";
 export { RouterProvider } from "./router-provider/router-provider.tsx";
