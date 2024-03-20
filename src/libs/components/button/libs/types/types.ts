404212e098d446644379904b1241e72d34a5f0c0
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-03-20 16:28:42 +0200

    feat: Create button component

---
+++ src/libs/components/button/libs/types/types.ts
@@ -0,0 +1,2 @@
+export { type ButtonType } from "./button-type.type.ts";
+export { type ButtonVariant } from "./button-variant.type.ts";
--- src/libs/components/components.ts
+++ src/libs/components/components.ts
@@ -1,4 +1,5 @@
 export { App } from "./app/app.tsx";
 export { Input } from "./input/input.tsx";
+export { Button } from "./button/button.tsx";
 export { PasswordInput } from "./password-input/password-input.tsx";
 export { RouterProvider } from "./router-provider/router-provider.tsx";
