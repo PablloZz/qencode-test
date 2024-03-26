f7e5fecf566f54d6c5e12caafe772e5b1704b97b
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-03-26 17:58:08 +0200

    fix: Move HttpHeader enum to Http package

--- src/libs/enums/enums.ts
+++ src/libs/enums/enums.ts
@@ -1,4 +1,3 @@
 export { ApiPath } from "./api-path.enum.ts";
 export { AppRoute } from "./app-route.enum.ts";
-export { HttpHeader } from "./http-header.enum.ts";
 export { ContentType } from "./content-type.enum.ts";
--- src/libs/packages/http/http.ts
+++ src/libs/packages/http/http.ts
@@ -3,6 +3,6 @@ import { Http } from "./http.package.js";
 const http = new Http();
 
 export { http };
-export { HttpCode } from "./libs/enums/enums.js";
+export { HttpCode, HttpHeader } from "./libs/enums/enums.js";
 export { HttpError } from "./libs/exceptions/exceptions.js";
 export { type THttp, type HttpOptions } from "./libs/types/types.js";
