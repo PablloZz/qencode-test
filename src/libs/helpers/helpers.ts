6a6909b50237cc35b9045a0b43fec4222d81db4d
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-03-26 18:02:11 +0200

    fix: Move DEFAULT_ERROR_MESSAGE to root constants

--- src/libs/helpers/helpers.ts
+++ src/libs/helpers/helpers.ts
@@ -1,2 +1,2 @@
+export { getErrorMessage } from "./get-error-message.helper.ts";
 export { getValidClassNames } from "./get-valid-class-names.helper.ts";
-export { getErrorMessage } from "./get-error-message/get-error-message.helper.ts";
