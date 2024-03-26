4af29d0fbd7e6aedfe0ae64890d5640db97a8389
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-03-26 17:56:50 +0200

    feat: Create ErrorBoundary component

--- src/libs/components/components.ts
+++ src/libs/components/components.ts
@@ -1,4 +1,5 @@
 export { App } from "./app/app.tsx";
 export { Input } from "./input/input.tsx";
 export { Button } from "./button/button.tsx";
+export { ErrorBoundary } from "./error-boundary/error-boundary.tsx";
 export { RouterProvider } from "./router-provider/router-provider.tsx";
